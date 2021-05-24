7af1af9912ab44240fc67e948fc2980eedec13df
Kowcika Asaithambi <kowci.mars@gmail.com>
2021-05-23 20:58:00 -0400

    Add project files.

---
+++ SystemAssignment3_Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SystemAssignment3_Singleton
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Singleton Board1 = Singleton.GetInstance;
+            Board1.PrintDetails("2 Players");
+
+            Singleton Board2 = Singleton.GetInstance;
+            Board2.PrintDetails("4 Players");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ SystemAssignment3_Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemAssignment3_Singleton
+{
+    public sealed class  Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value is " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
